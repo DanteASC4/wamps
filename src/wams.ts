b03d6695fda2d2dab69a34cd5ecc51146ba90f16
danteasc4 <zexovon@gmail.com>
2021-07-01 11:03:19 -0400

    License and comments

--- src/wams.ts
+++ src/wams.ts
@@ -1,18 +1,22 @@
 import path from 'path';
 import fs from 'fs-extra';
 
+// Read the calling directories package.json
 const packageJsonPath = path.join(process.cwd(), 'package.json');
 const asObj = fs.readJSONSync(packageJsonPath);
 const scriptsObj = asObj.scripts;
 
-// console.dir(scriptsObj);
+// Determine Longest Script Name for consistent spacing
 let longest = 'ScriptName'.length;
 for (const scriptName in scriptsObj) {
   if (scriptName.length > longest) longest = scriptName.length;
 }
 
+// Header
 console.log(`\n\x1b[33mScriptName |  \x1b[32mScript Command`);
 console.log('\x1b[36m————————————————————————————');
+
+// Logging the script names and commands with some simple colors and even spacing
 for (const scriptName in scriptsObj) {
   let numSpaces = Math.abs(scriptName.length - longest) + 1;
   let spaces = ' '.repeat(numSpaces);
